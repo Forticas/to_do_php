2c8a37a45918753ad60188766ab6b84d6a238699
forticas <houssem@forticas.com>
2022-11-18 11:02:21 +0100

    restriction accés aux fichers conf

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 $db_config = parse_ini_file('config/db.ini');
 
-
+var_dump($db_config);die;
 try {
     $pdo = new PDO(
         "mysql:host=".$db_config['db_host'].";dbname=".$db_config['db_name'],
